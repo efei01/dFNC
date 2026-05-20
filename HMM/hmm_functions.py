cd5e63add18369c4419c990299c73ca4bb24d613
efei01 <edward.fei2005@gmail.com>
2026-05-20 10:49:26 -0700

    Added hypothesis tests for transition probabilities in HMM/CIMT/CIMT_HMM_v2.ipynb

--- HMM/hmm_functions.py
+++ HMM/hmm_functions.py
@@ -863,3 +863,24 @@ def create_hypothesis_testing_dataframe(group_data: dict, group_labels: list, me
     
     ht_df = pd.concat(group_dfs, axis=0, ignore_index=True)
     return ht_df
+
+def separate_trans_probs(group_data: dict, group_labels: list, k: int):
+    """
+    Args:
+        group_data   : dict
+        group_labels : list
+        k            : int
+    Returns:
+        tp_df        : pandas DataFrame
+    """
+    group_dfs = []
+    for label in list(group_labels):
+        group_df = pd.DataFrame({
+            f'tp_{i + 1}to{j + 1}': list(group_data[label]['trans_matrices'][:, i, j]) for i in range(k) for j in range(k)
+        })
+        group_df['group'] = label
+        
+        group_dfs.append(group_df)
+        
+    tp_df = pd.concat(group_dfs, axis=0, ignore_index=True)
+    return tp_df
